f0654a0d40ce85601de9f0f824589624eabc5799
Tracy Dyer <Tracy.Dyer@lrs.com>
2018-01-10 22:02:42 -0600

    Changed valuescontroller to use the 3 stooges

--- GiftXchange/Controllers/ValuesController.cs
+++ GiftXchange/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace GiftXchange.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Moe", "Larry", "Curly" };
         }
 
         // GET api/values/5
