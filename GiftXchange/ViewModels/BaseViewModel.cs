54708b946f7407801c277be5fad36ffacdd374de
Tracy Dyer <Tracy.Dyer@lrs.com>
2018-01-22 12:08:05 -0600

    Added baseviewmodel

---
+++ GiftXchange/ViewModels/BaseViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiftXchange.ViewModels
+{
+    public class BaseViewModel
+    {
+    }
+}
