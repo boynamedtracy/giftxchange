fa75c3b9451af52302de42f57ccaefb1e15a5133
Tracy Dyer <Tracy.Dyer@lrs.com>
2018-01-23 17:09:03 -0600

    Added enum class

---
+++ GiftXchange/Helpers/Enums.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiftXchange.Helpers
+{
+  public class Enums
+  {
+    public enum GroupMemberStatus
+    {
+      Pending = 0,
+      Active = 1,
+      Suspended = 2
+    }
+  }
+}
